202247eafa53fd80a710ada13f073220df9baa94
Xavier Iafrate <x.iafrate@wixroydgroup.com>
2022-03-31 19:20:54 +0100

    started on fatal error screen

---
+++ ProjectLighthouse/View/ShowError.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ProjectLighthouse.View
+{
+    public partial class ShowError : Window, INotifyPropertyChanged
+    {
+        public DispatcherUnhandledExceptionEventArgs Error { get; set; }
+        public ShowError()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public void NotifyPropertyChanged()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Error)));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
