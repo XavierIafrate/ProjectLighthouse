a00aabef772a5eaf80dad9894d8ae6beef6cf021
Xavier Iafrate <x.iafrate@wixroydgroup.com>
2022-11-21 20:17:01 +0000

    Initial commit

---
+++ LighthouseMonitoring/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using LighthouseMonitoring.Model;
+using LighthouseMonitoring.ViewModel;
+using ProjectLighthouse;
+using ProjectLighthouse.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace LighthouseMonitoring
+{
+    public partial class App : Application
+    {
+        public static MainViewModel ViewModel { get; set; }
+        public const string DEBUG_ROOT = @"C:\Users\xavie\Documents\lighthouse_test\";
+        public const string DEBUG_DB_NAME = "manufactureDB.db3";
+
+        public static MonitoringSystem MonitoringSystem { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        public App()
+        {
+            EstablishDatabaseContext();
+
+            MonitoringSystem = new();
+            MonitoringSystem.Initialise();
+
+            MainWindow window = new();
+            ViewModel = new(window);
+        }
+
+        private void EstablishDatabaseContext()
+        {
+            
+            DatabaseHelper.DatabasePath = $"{ApplicationRootPaths.DEBUG_ROOT}{ApplicationRootPaths.DEBUG_DB_NAME}";
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Handled)
+            {
+                return;
+            }
+
+            // TODO
+
+            //RecordError(e);
+
+
+            //ShowError errorWindow = new() { Error = e };
+            //errorWindow.NotifyPropertyChanged();
+
+
+            //Window.Hide();
+            //errorWindow.ShowDialog();
+        }
+    }
+}
---
+++ LighthouseMonitoring/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using LighthouseMonitoring.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace LighthouseMonitoring.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+
+        public MainWindow MainWindow;
+
+		private BaseViewModel displayViewModel;
+		public BaseViewModel DisplayViewModel
+		{
+			get { return displayViewModel; }
+			set 
+			{ 
+				displayViewModel = value; 
+				OnPropertyChanged(); 
+			}
+		}
+
+		private Timer timer;
+
+		public MainViewModel(MainWindow window)
+		{
+
+			MainWindow = window;
+			MainWindow.DataContext = this;
+
+			DisplayText = "Controller";
+			
+			MainWindow.Show();
+
+			//timer = new(2500);
+			//timer.Elapsed += Timer_Elapsed;
+			//timer.Start();
+			DisplayViewModel = new MonitoringViewModel();
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			if (DisplayViewModel is MonitoringViewModel)
+			{
+				DisplayViewModel = new SettingsViewModel();
+			}
+			else
+			{
+				DisplayViewModel = new MonitoringViewModel();
+			}
+		}
+	}
+}
