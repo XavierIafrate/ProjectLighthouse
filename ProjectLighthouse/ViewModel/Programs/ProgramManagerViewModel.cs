1b4276fe8a34d3b5e433f7dfd07cff079e301bfe
Xavier Iafrate <xavieriafrate@gmail.com>
2023-03-11 22:43:40 +0000

    Added in semi-functional state

--- ProjectLighthouse/ViewModel/Programs/ProgramManagerViewModel.cs
+++ ProjectLighthouse/ViewModel/Programs/ProgramManagerViewModel.cs
@@ -1,15 +1,92 @@
-﻿using ProjectLighthouse.ViewModel.Core;
-using System;
+﻿using ProjectLighthouse.Model.Programs;
+using ProjectLighthouse.ViewModel.Core;
+using ProjectLighthouse.ViewModel.Helpers;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
-namespace ViewModel.Programs
+namespace ProjectLighthouse.ViewModel.Programs
 {
     public class ProgramManagerViewModel : BaseViewModel
     {
+        public List<NcProgram> Programs;
+
+        private List<NcProgram> filteredPrograms;
+        public List<NcProgram> FilteredPrograms
+        {
+            get { return filteredPrograms; }
+            set
+            {
+                filteredPrograms = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string searchString = "";
+        public string SearchString
+        {
+            get { return searchString; }
+            set
+            {
+                searchString = value;
+                Search();
+                OnPropertyChanged();
+            }
+        }
+
+        private NcProgram selectedProgram;
+        public NcProgram SelectedProgram
+        {
+            get { return selectedProgram; }
+            set
+            {
+                selectedProgram = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public ProgramManagerViewModel()
         {
+            LoadData();
+            Search();
+        }
 
+        void LoadData()
+        {
+            Programs = DatabaseHelper.Read<NcProgram>();
+            Programs.ForEach(x => x.ValidateAll());
+        }
+
+        void Search()
+        {
+            if (string.IsNullOrWhiteSpace(SearchString))
+            {
+                FilteredPrograms = new(Programs);
+                if (FilteredPrograms.Count > 0)
+                {
+                    SelectedProgram = filteredPrograms[0];
+                }
+                else
+                {
+                    SelectedProgram = null;
+                }
+                return;
+            }
+
+
+            FilteredPrograms = Programs
+                .Where(x => x.Name.ToLowerInvariant().Contains(SearchString.ToLowerInvariant()))
+                .ToList();
+
+            if (FilteredPrograms.Count > 0)
+            {
+                SelectedProgram = Programs[0];
+            }
+            else
+            {
+                SelectedProgram = null;
+            }
         }
     }
 }
+
