1507046f07d92d4e14d8059e1ff6eb9e9031cd43
Xavier Iafrate <xavieriafrate@gmail.com>
2023-03-18 19:08:23 +0000

    New Converter

---
+++ ProjectLighthouse/ViewModel/ValueConverters/divideDouble.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectLighthouse.ViewModel.ValueConverters
+{
+    public class divideDouble : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double val) throw new Exception($"doubleToHalfValue Convert: Expected double not received (value)");
+            if (!double.TryParse(parameter.ToString(), out double par))
+            {
+                throw new Exception($"doubleToHalfValue Convert: Expected double not received (parameter)");
+            }
+
+
+            return val / par;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not double val) throw new Exception($"doubleToHalfValue ConvertBack: Expected double not received (value)");
+            if (parameter is not double par) throw new Exception($"doubleToHalfValue ConvertBack: Expected double not received (parameter)");
+
+            return val * par;
+        }
+    }
+}
