fe6990adbc58ac4b28b0ea32d55bb9cc47f92606
Xavier Iafrate <xavieriafrate@gmail.com>
2023-03-29 15:55:55 +0100

    Refactor

--- ProjectLighthouse/View/UserControls/DisplayDrawingGroup.xaml.cs
+++ ProjectLighthouse/View/UserControls/DisplayDrawingGroup.xaml.cs
@@ -1,32 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ProjectLighthouse.Model.Drawings;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ProjectLighthouse.Model.Drawings;
 
 namespace ProjectLighthouse.View.UserControls
 {
     public partial class DisplayDrawingGroup : UserControl
     {
-
-
         public TechnicalDrawingGroup Group
         {
             get { return (TechnicalDrawingGroup)GetValue(GroupProperty); }
             set { SetValue(GroupProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for Group.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty GroupProperty =
             DependencyProperty.Register("Group", typeof(TechnicalDrawingGroup), typeof(DisplayDrawingGroup), new PropertyMetadata(null, SetValues));
 
@@ -37,14 +23,14 @@ namespace ProjectLighthouse.View.UserControls
                 return;
             }
 
-            if(control.Group == null)
+            if (control.Group == null)
             {
                 return;
             }
 
-            control.ActionRequiredBadge.Visibility = control.Group.Drawings.Where(x => !x.IsApproved && !x.IsRejected && !x.IsWithdrawn).Count() == 0
-                ? Visibility.Collapsed
-                : Visibility.Visible;
+            control.ActionRequiredBadge.Visibility = control.Group.Drawings.Any(x => x.PendingApproval())
+                ? Visibility.Visible
+                : Visibility.Collapsed;
 
             control.WithdrawnText.Visibility = control.Group.AllDrawingsWithdrawn
                 ? Visibility.Visible
