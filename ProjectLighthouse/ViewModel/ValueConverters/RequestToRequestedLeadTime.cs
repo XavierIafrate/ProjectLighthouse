1eac56b526c45007f25be34ae0a4e9e14264c1c1
Xavier Iafrate <xavieriafrate@gmail.com>
2023-03-31 16:19:11 +0100

    New lead time display

---
+++ ProjectLighthouse/ViewModel/ValueConverters/RequestToRequestedLeadTime.cs
@@ -0,0 +1,27 @@
+﻿using ProjectLighthouse.Model.Requests;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectLighthouse.ViewModel.ValueConverters
+{
+    public class RequestToRequestedLeadTime : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Request request) throw new ArgumentException("Request expected");
+
+            double weeks = (request.DateRequired - request.DateRaised).TotalDays / 7;
+            weeks *= 2;
+            weeks = Math.Round(weeks);
+            weeks /= 2;
+
+            return weeks;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
