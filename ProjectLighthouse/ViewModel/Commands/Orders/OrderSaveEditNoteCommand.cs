f71ddd02a3c943e00358a6aa4eeb6da96b2f634a
Xavier Iafrate <xavieriafrate@gmail.com>
2023-05-19 11:32:24 +0100

    New commands

---
+++ ProjectLighthouse/ViewModel/Commands/Orders/OrderSaveEditNoteCommand.cs
@@ -0,0 +1,29 @@
+﻿using ProjectLighthouse.Model.Core;
+using ProjectLighthouse.View.Orders;
+using System;
+using System.Windows.Input;
+
+namespace ProjectLighthouse.ViewModel.Commands.Orders
+{
+    public class OrderSaveEditNoteCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private EditLMOWindow window;
+        public OrderSaveEditNoteCommand(EditLMOWindow w)
+        {
+            this.window = w;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is not Note note) return;
+            window.SaveNoteEdit(note);
+        }
+    }
+}
---
+++ ProjectLighthouse/ViewModel/Commands/Scheduling/ManageHolidaysCommand.cs
@@ -0,0 +1,28 @@
+﻿using ProjectLighthouse.Model.Administration;
+using ProjectLighthouse.ViewModel.Orders;
+using System;
+using System.Windows.Input;
+
+namespace ProjectLighthouse.ViewModel.Commands.Scheduling
+{
+    public class ManageHolidaysCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private ScheduleViewModel viewModel;
+
+        public ManageHolidaysCommand(ScheduleViewModel vm)
+        {
+            this.viewModel = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return App.CurrentUser.Role >= UserRole.Scheduling;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.EditHolidays();
+        }
+    }
+}
