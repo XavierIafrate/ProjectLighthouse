521c767f1f5cb3f25942109bdb483aab4f185322
Xavier Iafrate <xavieriafrate@gmail.com>
2023-07-26 15:07:05 +0100

    New Command

---
+++ ProjectLighthouse/ViewModel/Commands/Orders/ShowWorkloadWindowCommand.cs
@@ -0,0 +1,26 @@
+﻿using ProjectLighthouse.ViewModel.Orders;
+using System;
+using System.Windows.Input;
+
+namespace ViewModel.Commands.Orders
+{
+    public class ShowWorkloadWindowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private OrderViewModel viewModel;
+
+        public ShowWorkloadWindowCommand(OrderViewModel vm)
+        {
+            this.viewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.ShowWorkload();
+        }
+    }
+}
