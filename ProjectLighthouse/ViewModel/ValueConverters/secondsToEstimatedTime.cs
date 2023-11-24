f764cf51055fb2b76eef237a42fa4d69b6f48588
Xavier Iafrate <xavieriafrate@gmail.com>
2023-11-24 21:37:31 +0000

    New converter

---
+++ ProjectLighthouse/ViewModel/ValueConverters/secondsToEstimatedTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectLighthouse.ViewModel.ValueConverters
+{
+    public class secondsToEstimatedTime : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string result = "Error";
+
+            if (value is int seconds)
+            {
+                TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+                if (timeSpan.TotalHours < 1)
+                {
+                    result = $"{timeSpan.Minutes}m";
+                }
+                else if (timeSpan.TotalDays < 1)
+                {
+                    result = $"{timeSpan.Hours}h {timeSpan.Minutes}m";
+                }
+                else
+                {
+                    result = $"{timeSpan.Days}d {timeSpan.Hours}h";
+                }
+
+                return result;
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
