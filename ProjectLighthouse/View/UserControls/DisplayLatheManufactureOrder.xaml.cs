6db1c78098f3a5c8a255fa82c32757c5e0e93bbd
Xavier Iafrate <xavieriafrate@gmail.com>
2023-12-17 15:52:52 +0000

    Remove binding, tweak format

--- ProjectLighthouse/View/UserControls/DisplayLatheManufactureOrder.xaml.cs
+++ ProjectLighthouse/View/UserControls/DisplayLatheManufactureOrder.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using ProjectLighthouse.Model.Orders;
+using ProjectLighthouse.ViewModel.ValueConverters;
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ProjectLighthouse.View.UserControls
 {
@@ -21,9 +23,14 @@ namespace ProjectLighthouse.View.UserControls
             if (d is not DisplayLatheManufactureOrder control)
                 return;
 
-            control.DataContext = control.LatheManufactureOrder;
+            if (control.LatheManufactureOrder == null) return;
 
-            control.RecentUpdateFlag.Visibility = (control.LatheManufactureOrder.ModifiedAt.AddHours(5) > System.DateTime.Now)
+            control.OrderNameTextBlock.Text = control.LatheManufactureOrder.Name;
+            control.AllocatedMachineTextBlock.Text = control.LatheManufactureOrder.AllocatedMachine;
+            control.PoTextBlock.Text = control.LatheManufactureOrder.POReference;
+            control.AssignmentTextBlock.Text = control.LatheManufactureOrder.AssignedTo;
+
+            control.RecentUpdateFlag.Visibility = (control.LatheManufactureOrder.ModifiedAt.AddHours(5) > DateTime.Now)
                 && control.LatheManufactureOrder.State < OrderState.Running
                 ? Visibility.Visible
                 : Visibility.Collapsed;
@@ -32,7 +39,20 @@ namespace ProjectLighthouse.View.UserControls
                    && control.LatheManufactureOrder.State == OrderState.Problem
                    && control.LatheManufactureOrder.CreatedAt.AddDays(5) < DateTime.Now;
 
-            control.BadgeText.Visibility = control.LatheManufactureOrder.IsClosed || needsUpdate ? Visibility.Collapsed : Visibility.Visible;
+            control.StaleBadge.Visibility = 
+                       control.LatheManufactureOrder.ModifiedAt.AddDays(5) < DateTime.Now
+                    && control.LatheManufactureOrder.State == OrderState.Problem
+                    && control.LatheManufactureOrder.CreatedAt.AddDays(5) < DateTime.Now 
+                    && control.LatheManufactureOrder.State == OrderState.Problem 
+                            ? Visibility.Visible    
+                            : Visibility.Collapsed;
+
+            control.StatusBadgeText.Text = control.LatheManufactureOrder.State.ToString();
+            control.StatusBadgeText.Visibility = control.LatheManufactureOrder.IsClosed || needsUpdate ? Visibility.Collapsed : Visibility.Visible;
+            OrderStateToBrush converter = new();
+            control.StatusBadge.Background = converter.Convert(control.LatheManufactureOrder.State, null, "faded", null) as Brush;
+            control.StatusBadgeText.Foreground = converter.Convert(control.LatheManufactureOrder.State, null, null, null) as Brush;
+            control.ClosedBadge.Visibility = control.LatheManufactureOrder.IsClosed ? Visibility.Visible : Visibility.Collapsed;
 
             control.PoTextBlock.Visibility = App.CurrentUser.Role is Model.Administration.UserRole.Purchasing
                 ? Visibility.Visible
@@ -41,6 +61,7 @@ namespace ProjectLighthouse.View.UserControls
             control.clippy.Visibility = control.PoTextBlock.Visibility == Visibility.Visible || control.LatheManufactureOrder.AssignedTo is null
                 ? Visibility.Collapsed
                 : Visibility.Visible;
+
             control.AssignmentTextBlock.Visibility = control.PoTextBlock.Visibility == Visibility.Visible || control.LatheManufactureOrder.AssignedTo is null
                 ? Visibility.Collapsed
                 : Visibility.Visible;
