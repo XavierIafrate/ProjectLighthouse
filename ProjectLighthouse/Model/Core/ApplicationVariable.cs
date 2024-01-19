e1db250146b598dfe65a8d66a6b8016fb0585908
Xavier Iafrate <xavieriafrate@gmail.com>
2024-01-19 14:33:19 +0000

    new general purpose data storage

---
+++ ProjectLighthouse/Model/Core/ApplicationVariable.cs
@@ -0,0 +1,49 @@
+﻿using SQLite;
+using System;
+using System.Diagnostics;
+
+namespace ProjectLighthouse.Model.Core
+{
+    public class ApplicationVariable
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+
+        [NotNull]
+        public string DataType { get; set; }
+
+        public string StringValue { get; set; }
+
+        [Ignore]
+        public object Data
+        {
+            get
+            {
+                return DataType switch
+                {
+                    "null" => null,
+                    "DateTime" => DateTime.Parse(StringValue),
+                    "string" => StringValue,
+                    "int" => int.Parse(StringValue),
+                    "double" => double.Parse(StringValue),
+                    "bool" => bool.Parse(StringValue),
+                    _ => "Unknown Data"
+                };
+            }
+            set
+            {
+                if(value == null)
+                {
+                    DataType = "null";
+                    StringValue = null;
+                    return;
+                }
+
+                DataType = value.GetType().Name;
+                Debug.WriteLine(DataType);
+                StringValue = value.ToString();
+            }
+        }
+
+    }
+}
