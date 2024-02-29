693860b33a2d66799a0cc5b30bbf5e659cf23439
Xavier Iafrate <xavieriafrate@gmail.com>
2024-02-29 10:34:34 +0000

    Added clone method

--- ProjectLighthouse/Model/Scheduling/MachineService.cs
+++ ProjectLighthouse/Model/Scheduling/MachineService.cs
@@ -2,6 +2,10 @@
 {
     public class MachineService : ScheduleItem
     {
-
+        internal MachineService Clone()
+        {
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(this);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<MachineService>(json);
+        }
     }
 }
