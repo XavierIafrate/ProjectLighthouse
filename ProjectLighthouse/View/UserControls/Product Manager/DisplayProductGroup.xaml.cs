beaf87a98d32e612aa0ad5e8b40db0f64963b324
Xavier Iafrate <xavieriafrate@gmail.com>
2024-03-01 13:01:18 +0000

    Cleanup

--- ProjectLighthouse/View/UserControls/Product Manager/DisplayProductGroup.xaml.cs
+++ ProjectLighthouse/View/UserControls/Product Manager/DisplayProductGroup.xaml.cs
@@ -6,15 +6,12 @@ namespace ProjectLighthouse.View.UserControls
 {
     public partial class DisplayProductGroup : UserControl
     {
-
-
         public ProductGroup ProductGroup
         {
             get { return (ProductGroup)GetValue(ProductGroupProperty); }
             set { SetValue(ProductGroupProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for ProductGroup.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ProductGroupProperty =
             DependencyProperty.Register("ProductGroup", typeof(ProductGroup), typeof(DisplayProductGroup), new PropertyMetadata(null, SetValues));
 
