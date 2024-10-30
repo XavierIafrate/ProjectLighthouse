d2bc10c1573430dd2a9f13e960de504aaf8cf167
Xavier Iafrate <xavieriafrate@gmail.com>
2024-10-30 11:09:09 +0000

    New server side process

---
+++ CloseManufactureOrders/ApplicationRootPaths.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLighthouse
+{
+    public class ApplicationRootPaths
+    {
+        public const string DEBUG_ROOT = @"\\groupfile01\Sales\Production\Administration\Manufacture Records\Lighthouse_TEST\";
+        public const string DEBUG_DB_NAME = @"lightroom_TEST.db3";
+
+        public const string RELEASE_ROOT = @"\\groupfile01\Sales\Production\Administration\Manufacture Records\Lighthouse\";
+        public const string RELEASE_DB_NAME = @"lightroom_v2.db3";
+
+        public const string DEMO_DB_NAME = @"lightroom_v2_demo.db3";
+    }
+}
