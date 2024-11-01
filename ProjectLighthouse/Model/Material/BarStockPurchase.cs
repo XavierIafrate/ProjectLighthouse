0d68fb20ce67196b30a05fdfe9b90161e19e64f9
Xavier Iafrate <xavieriafrate@gmail.com>
2024-11-01 13:21:04 +0000

    refactor

--- ProjectLighthouse/Model/Material/BarStockPurchase.cs
+++ ProjectLighthouse/Model/Material/BarStockPurchase.cs
@@ -1,7 +1,5 @@
 ﻿using SQLite;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ProjectLighthouse.Model.Material
 {
@@ -11,8 +9,8 @@ namespace ProjectLighthouse.Model.Material
         public int Id { get; set; }
         public Int64 OperaId { get; set; }
 
-        public string PurchaseOrder { get; set; } 
-        public DateTime DateRequired { get; set; } 
+        public string PurchaseOrder { get; set; }
+        public DateTime DateRequired { get; set; }
 
         public string BarId { get; set; }
         public int QuantityRequired { get; set; }
@@ -24,7 +22,7 @@ namespace ProjectLighthouse.Model.Material
 
         internal bool IsUpdated(BarStockPurchase newData)
         {
-            if(OperaId != newData.OperaId)
+            if (OperaId != newData.OperaId)
             {
                 throw new ArgumentException("Opera IDs must match.");
             }
