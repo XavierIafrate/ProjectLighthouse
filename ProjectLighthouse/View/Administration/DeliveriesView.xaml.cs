b81645dd83a0ad6bee9f415a7bdde8f386726ea0
Xavier Iafrate <xavieriafrate@gmail.com>
2024-11-01 13:23:02 +0000

    Review of deliveries process

--- ProjectLighthouse/View/Administration/DeliveriesView.xaml.cs
+++ ProjectLighthouse/View/Administration/DeliveriesView.xaml.cs
@@ -7,11 +7,19 @@ namespace ProjectLighthouse.View.Administration
         public DeliveriesView()
         {
             InitializeComponent();
+            EditItemButton.Visibility = App.CurrentUser.HasPermission(Model.Core.PermissionType.EditDelivery) 
+                ? System.Windows.Visibility.Visible 
+                : System.Windows.Visibility.Collapsed;
         }
 
         private void clearButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             searchBox.Text = "";
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.scroller.ScrollToTop();
         }
     }
 }
---
+++ ProjectLighthouse/View/Administration/EditDeliveryItemWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using ProjectLighthouse.Model.Deliveries;
+using ProjectLighthouse.ViewModel.Helpers;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace ProjectLighthouse.View.Administration
+{
+    public partial class EditDeliveryItemWindow : Window, INotifyPropertyChanged
+    {
+        public bool SaveExit { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DeliveryItem originalItem;
+
+
+        private DeliveryItem item;
+        public DeliveryItem Item
+        {
+            get { return item; }
+            set { item = value; OnPropertyChanged(); }
+        }
+
+
+        public EditDeliveryItemWindow(DeliveryItem item)
+        {
+            InitializeComponent();
+
+            this.originalItem = item;
+            this.Item = item.Clone() as DeliveryItem;
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Item.PurchaseOrderReference == originalItem.PurchaseOrderReference && item.ExportProductName == originalItem.ExportProductName)
+            {
+                Close();
+                return;
+            }
+
+            try
+            {
+                Item.EditedAt = DateTime.Now;
+                Item.EditedBy = App.CurrentUser.UserName;
+                bool success = DatabaseHelper.Update(Item, throwErrs: true);
+
+                if (!success)
+                {
+                    MessageBox.Show("Failed to update the delivery item.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    SaveExit = true;
+                    originalItem = Item;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Failed to update the delivery item:" + Environment.NewLine + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
