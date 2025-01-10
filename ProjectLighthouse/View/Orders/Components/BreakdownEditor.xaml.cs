c03ee2641408381dc950eca97ab0eed8b812738d
Xavier Iafrate <xavieriafrate@gmail.com>
2025-01-10 16:11:03 +0000

    dead feature - saving

--- ProjectLighthouse/View/Orders/Components/BreakdownEditor.xaml.cs
+++ ProjectLighthouse/View/Orders/Components/BreakdownEditor.xaml.cs
@@ -40,10 +40,21 @@ namespace ProjectLighthouse.View.Orders.Components
         }
 
         public static readonly DependencyProperty OrderProperty =
-            DependencyProperty.Register("Order", typeof(LatheManufactureOrder), typeof(BreakdownEditor), new PropertyMetadata(null));
+            DependencyProperty.Register("Order", typeof(LatheManufactureOrder), typeof(BreakdownEditor), new PropertyMetadata(null, ReDraw));
 
+        private static void ReDraw(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not BreakdownEditor control) return;
+            if (control.Order is null) return;
+            control.Draw();
+            DateTime now = new();
+            bool runningNowBySchedule = now > control.Order.StartDate && now < control.Order.EndsAt();
 
-        private MachineBreakdown newBreakdown = new();
+            control.NewBreakdown.BreakdownEnded = runningNowBySchedule ? now.ChangeTime(now.Hour, 0, 0, 0) : control.Order.EndsAt().ChangeTime(control.Order.EndsAt().Hour, 0, 0, 0);
+            control.NewBreakdown.BreakdownStarted = control.NewBreakdown.BreakdownEnded.AddHours(-1) > control.Order.StartDate ? control.NewBreakdown.BreakdownEnded.AddHours(-1) : control.Order.StartDate;
+        }
+
+        private MachineBreakdown newBreakdown;
 
         public MachineBreakdown NewBreakdown
         {
@@ -62,15 +73,17 @@ namespace ProjectLighthouse.View.Orders.Components
         public BreakdownEditor()
         {
             InitializeComponent();
+            NewBreakdown = new();
+
+            NewBreakdown.PropertyChanged += OnNewBreakdownPropertyChanged;
+
             breakdownCodes = DatabaseHelper.Read<BreakdownCode>();
             this.BreakdownCodes_ComboBox.ItemsSource = this.breakdownCodes;
             if (this.breakdownCodes.Count > 0)
             {
                 this.BreakdownCodes_ComboBox.SelectedIndex = 0;
             }
-            DateTime now = DateTime.Now;
-            NewBreakdown.BreakdownEnded = now.ChangeTime(now.Hour,0,0,0);
-            NewBreakdown.BreakdownStarted = NewBreakdown.BreakdownEnded.AddHours(-1);
+
             if (breakdownCodes.Count > 0)
             {
                 NewBreakdown.BreakdownCode = breakdownCodes.First().Id;
@@ -98,7 +111,10 @@ namespace ProjectLighthouse.View.Orders.Components
 
             Order.Breakdowns = Order.Breakdowns.Append((MachineBreakdown)NewBreakdown.Clone()).ToList();
 
+            NewBreakdown.PropertyChanged -= OnNewBreakdownPropertyChanged;
             NewBreakdown = new();
+            NewBreakdown.PropertyChanged += OnNewBreakdownPropertyChanged;
+
             DateTime now = DateTime.Now;
             NewBreakdown.BreakdownEnded = now.ChangeTime(now.Hour, 0, 0, 0);
             NewBreakdown.BreakdownStarted = NewBreakdown.BreakdownEnded.AddHours(-1);
@@ -108,5 +124,104 @@ namespace ProjectLighthouse.View.Orders.Components
                 NewBreakdown.BreakdownCode = breakdownCodes.First().Id;
             }
         }
+
+        private void OnNewBreakdownPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void BreakdownViewGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void Draw()
+        {
+            if (Order is null) return;
+            if (!breakdownOverlayGrid.IsLoaded || !BreakdownViewGrid.IsLoaded) return;
+
+            breakdownOverlayGrid.Children.Clear();
+            newBreakdownOverlayGrid.Children.Clear();
+
+
+            DateTime start = Order.GetSettingStartDateTime();
+            DateTime end = Order.EndsAt();
+            TimeSpan overrun = TimeSpan.Zero;
+
+            if (NewBreakdown.BreakdownEnded > Order.EndsAt())
+            {
+                overrun = NewBreakdown.BreakdownEnded - Order.EndsAt();
+                end = NewBreakdown.BreakdownEnded;
+            }
+
+
+            TimeSpan coverage = end - start;
+
+            if (coverage.TotalMinutes == 0) return;
+
+            double pxPerMinute = BreakdownViewGrid.ActualWidth / coverage.TotalMinutes;
+
+            BreakdownViewGrid.ColumnDefinitions[0].Width = new(pxPerMinute * Order.TimeToSet * 60);
+            BreakdownViewGrid.ColumnDefinitions[2].Width = new(pxPerMinute * overrun.TotalMinutes);
+
+
+            testTextBlock.Text = $"{coverage.TotalMinutes:0} mins {Order.TimeToSet * 60:0} setting mins";
+            testTextBlock.Text += Environment.NewLine + $"Order running {Order.StartDate} to {Order.EndsAt()}";
+            testTextBlock.Text += Environment.NewLine + $"pxPerMin: {pxPerMinute:0.000}";
+
+            for (int i = 0; i < Order.Breakdowns.Count; i++)
+            {
+                MachineBreakdown breakdown = Order.Breakdowns[i];
+                double lMargin = (breakdown.BreakdownStarted - Order.StartDate).TotalMinutes * pxPerMinute;
+                double rMargin = (Order.EndsAt() - breakdown.BreakdownEnded).TotalMinutes * pxPerMinute;
+                double width = (breakdown.BreakdownEnded - breakdown.BreakdownStarted).TotalMinutes; // Doesn't add up!
+                width *= pxPerMinute;
+
+                DisplayBreakdown breakdownGraphic = new()
+                {
+                    CodeText = breakdown.BreakdownCode,
+                    Width = width,
+                    Margin = new Thickness(lMargin, 0, 0, 0),
+                };
+
+                AddToGrid(breakdownOverlayGrid, breakdownGraphic, 0, 0);
+                testTextBlock.Text += Environment.NewLine + $"breakdown #{i + 1} left margin {lMargin.ToString("0.0")}";
+                testTextBlock.Text += Environment.NewLine + $"breakdown #{i + 1} right margin {rMargin.ToString("0.0")}";
+                testTextBlock.Text += Environment.NewLine + $"breakdown #{i + 1} width {width.ToString("0.0")}";
+                testTextBlock.Text += Environment.NewLine + $"breakdown #{i + 1} duration {(breakdown.BreakdownEnded - breakdown.BreakdownStarted).TotalMinutes.ToString("0.0")}";
+                testTextBlock.Text += Environment.NewLine + $"breakdown #{i + 1} started {breakdown.BreakdownStarted}";
+                testTextBlock.Text += Environment.NewLine + $"breakdown #{i + 1} ended {breakdown.BreakdownEnded}";
+            }
+
+            double width2 = Math.Max((NewBreakdown.BreakdownEnded - NewBreakdown.BreakdownStarted).TotalMinutes * pxPerMinute, 0);
+            double lMargin2 = (NewBreakdown.BreakdownStarted - Order.StartDate).TotalMinutes * pxPerMinute;
+
+            DisplayBreakdown newBreakdownGraphic = new()
+            {
+                CodeText = NewBreakdown.BreakdownCode,
+                Width = width2,
+                Margin = new Thickness(lMargin2, 0, 0, 0),
+            };
+
+            newBreakdownGraphic.SetPending(true);
+
+            AddToGrid(newBreakdownOverlayGrid, newBreakdownGraphic, 0, 0);
+
+        }
+
+        private static void AddToGrid(Grid grid, UIElement control, int column, int row, int colSpan = 1, int rowSpan = 1)
+        {
+            grid.Children.Add(control);
+            Grid.SetColumn(control, column);
+            Grid.SetRow(control, row);
+            Grid.SetColumnSpan(control, colSpan);
+            Grid.SetRowSpan(control, rowSpan);
+        }
+
+        private void BreakdownViewGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+
     }
 }
---
+++ ProjectLighthouse/View/Orders/Components/DisplayBreakdown.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ProjectLighthouse.View.Orders.Components
+{
+    public partial class DisplayBreakdown : UserControl
+    {
+        public string CodeText
+        {
+            get { return (string)GetValue(CodeTextProperty); }
+            set { SetValue(CodeTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty CodeTextProperty =
+            DependencyProperty.Register("CodeText", typeof(string), typeof(DisplayBreakdown), new PropertyMetadata(string.Empty));
+
+
+
+        public DisplayBreakdown()
+        {
+            InitializeComponent();
+        }
+
+        public void SetPending(bool pending) 
+        {
+            Brush fadedBrush = (Brush)App.Current.Resources[pending ? "OrangeFaded" : "RedFaded"];
+            Brush solidBrush = (Brush)App.Current.Resources[pending ? "Orange" : "Red"];
+            Brush foregroundBrush = (Brush)App.Current.Resources[pending ? "OnOrange" : "OnRed"];
+
+            breakdown_border.Background = fadedBrush;
+            breakdown_border.BorderBrush = solidBrush;
+            borderPattern.Stroke = solidBrush;
+            label.Background= solidBrush;
+            labelText.Foreground= foregroundBrush;
+        }
+    }
+}
