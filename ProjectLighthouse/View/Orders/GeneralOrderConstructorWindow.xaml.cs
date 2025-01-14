15756768ca39157e179d1620b5497f4bfec16cb5
Xavier Iafrate <xavieriafrate@gmail.com>
2025-01-14 14:37:01 +0000

    Added new product and edit functions

--- ProjectLighthouse/View/Orders/GeneralOrderConstructorWindow.xaml.cs
+++ ProjectLighthouse/View/Orders/GeneralOrderConstructorWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ProjectLighthouse.Model.Orders;
 using ProjectLighthouse.Model.Products;
+using ProjectLighthouse.ViewModel.Core;
 using ProjectLighthouse.ViewModel.Helpers;
 using System;
 using System.Collections.Generic;
@@ -31,10 +32,21 @@ namespace ProjectLighthouse.View.Orders
 
         public bool SaveExit { get; set; }
 
+        private NonTurnedItem newItem;
+
+        public NonTurnedItem NewItem
+        {
+            get { return newItem; }
+            set { newItem = value; OnPropertyChanged(); }
+        }
+
+
         public GeneralOrderConstructorWindow()
         {
             InitializeComponent();
             NewOrder = new();
+            NewItem = new();
+
             Items = DatabaseHelper.Read<NonTurnedItem>();
 
             if (Items.Count == 0)
@@ -46,6 +58,8 @@ namespace ProjectLighthouse.View.Orders
             DatePicker.SelectedDate = DateTime.Today.AddMonths(1);
 
             NewOrder.NonTurnedItemId = Items.First().Id;
+
+            this.UpdateProductControls.Visibility = Visibility.Collapsed;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -94,6 +108,96 @@ namespace ProjectLighthouse.View.Orders
             const string blank = "G00000";
 
             return blank[..(6 - orderNumLen)] + strOrderNum;
+        }
+
+        private void CreateProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewItem.ValidateAll();
+            if (NewItem.HasErrors)
+            {
+                return;
+            }
+
+            try
+            {
+                DatabaseHelper.Insert(NewItem, throwErrs: true);
+            }
+            catch(Exception ex)
+            {
+                NotificationManager.NotifyHandledException(ex);
+                return;
+            }
+
+            Items = DatabaseHelper.Read<NonTurnedItem>();
+            NewOrder.NonTurnedItemId = NewItem.Id;
+
+            NewItem = new();
+        }
+
+        private void SaveProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewItem.ValidateAll();
+            if (NewItem.HasErrors)
+            {
+                return;
+            }
+
+            try
+            {
+                DatabaseHelper.Update(NewItem, throwErrs: true);
+            }
+            catch (Exception ex)
+            {
+                NotificationManager.NotifyHandledException(ex);
+                return;
+            }
+
+            Items = DatabaseHelper.Read<NonTurnedItem>();
+            NewOrder.NonTurnedItemId = NewItem.Id;
+            ExitEdit();
+        }
+
+        private void CancelProductChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExitEdit();
+        }
+
+        private void EditProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NewOrder.NonTurnedItemId == 0)
+                return;
+
+            EnterEdit();
+        }
+
+        private void EnterEdit()
+        {
+            EditProductButton.IsEnabled = false;
+            NonTurnedItem? itemToEdit = Items.Find(x => x.Id == NewOrder.NonTurnedItemId);
+
+            if(itemToEdit == null)
+            {
+                MessageBox.Show($"Could not find item with Id {NewOrder.NonTurnedItemId}");
+                return;
+            }
+
+            NewItem = (NonTurnedItem)itemToEdit.Clone();
+            ProductEditHeader.Text = "Edit Product";
+            CreateProductButton.Visibility = Visibility.Collapsed;
+            UpdateProductControls.Visibility = Visibility.Visible;
+            ProductNameInput.IsEnabled = false;
+
+
+        }
+
+        private void ExitEdit()
+        {
+            ProductEditHeader.Text = "New Product";
+            NewItem = new();
+            CreateProductButton.Visibility = Visibility.Visible;
+            UpdateProductControls.Visibility = Visibility.Collapsed;
+            ProductNameInput.IsEnabled = true;
+            EditProductButton.IsEnabled = true;
         }
     }
 }
