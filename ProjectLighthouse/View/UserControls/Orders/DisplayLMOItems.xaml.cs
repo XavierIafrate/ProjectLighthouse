60a8516d2ecfefd536719a6499ebb4c4c8f5244e
Xavier Iafrate <xavieriafrate@gmail.com>
2025-01-15 12:23:49 +0000

    Add no cycle time indicator

--- ProjectLighthouse/View/UserControls/Orders/DisplayLMOItems.xaml.cs
+++ ProjectLighthouse/View/UserControls/Orders/DisplayLMOItems.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using ProjectLighthouse.Model.Orders;
-using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 
 namespace ProjectLighthouse.View.UserControls
 {
@@ -20,95 +17,10 @@ namespace ProjectLighthouse.View.UserControls
 
         private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            //if (d is not DisplayLMOItems control) return;
-            
-            ////control.DataContext = control.LatheManufactureOrderItem;
+            if (d is not DisplayLMOItems control) return;
 
-            //control.requirements.BorderBrush = (control.LatheManufactureOrderItem.QuantityDelivered >= control.LatheManufactureOrderItem.RequiredQuantity) ? // Customer requirement fulfilled
-            //    (Brush)Application.Current.Resources["Green"] : (Brush)Application.Current.Resources["Red"];
-
-            //control.requirementsBg.Background = (control.LatheManufactureOrderItem.QuantityDelivered >= control.LatheManufactureOrderItem.RequiredQuantity) ? // Customer requirement fulfilled
-            //    (Brush)Application.Current.Resources["Green"] : (Brush)Application.Current.Resources["Red"];
-
-            //control.qtyrequired.Foreground = (control.LatheManufactureOrderItem.QuantityDelivered >= control.LatheManufactureOrderItem.RequiredQuantity) ? // Customer requirement fulfilled
-            //    (Brush)Application.Current.Resources["Green"] : (Brush)Application.Current.Resources["Red"];
-
-            //control.daterequired.Foreground = (control.LatheManufactureOrderItem.QuantityDelivered >= control.LatheManufactureOrderItem.RequiredQuantity) ? // Customer requirement fulfilled
-            //    (Brush)Application.Current.Resources["Green"] : (Brush)Application.Current.Resources["Red"];
-
-            //control.custReqTitle.Foreground = (control.LatheManufactureOrderItem.QuantityDelivered >= control.LatheManufactureOrderItem.RequiredQuantity) ? // Customer requirement fulfilled
-            //    (Brush)Application.Current.Resources["Green"] : (Brush)Application.Current.Resources["Red"];
-
-            ////control.EditButton.Visibility = control.LatheManufactureOrderItem.ShowEdit
-            ////    ? Visibility.Visible
-            ////    : Visibility.Collapsed;
-            ///
-
-
-
-
-
-
-            //control.cycleTimeIndicator.Visibility = control.LatheManufactureOrderItem.QuantityMade > 0 || control.LatheManufactureOrderItem.CycleTime > 0
-            //    ? Visibility.Visible
-            //    : Visibility.Collapsed;
-
-            //control.CycleTimeText.Text = control.LatheManufactureOrderItem.CycleTime == 0
-            //    ? "?"
-            //    : $"{Math.Floor((double)control.LatheManufactureOrderItem.CycleTime / 60):0}m {control.LatheManufactureOrderItem.CycleTime % 60}s";
-
-            //if (control.cycleTimeIndicator.Visibility == Visibility.Visible && control.LatheManufactureOrderItem.CycleTime == 0)
-            //{
-            //    control.cycleTimeIndicator.Background = (Brush)Application.Current.Resources["RedFaded"];
-            //    control.cycleTimeIndicatorIcon.Fill = (Brush)Application.Current.Resources["Red"];
-            //    control.CycleTimeText.Foreground = (Brush)Application.Current.Resources["Red"];
-            //}
-            //else
-            //{
-            //    control.cycleTimeIndicator.Background = (Brush)Application.Current.Resources["TealFaded"];
-            //    control.cycleTimeIndicatorIcon.Fill = (Brush)Application.Current.Resources["Teal"];
-            //    control.CycleTimeText.Foreground = (Brush)Application.Current.Resources["Teal"];
-            //}
-
-
-
-
-            //if (control.LatheManufactureOrderItem.PreviousCycleTime is null)
-            //{
-            //    control.deltaIndicator.Visibility = Visibility.Collapsed;
-            //}
-            //else if (control.LatheManufactureOrderItem.CycleTime > 0 && control.LatheManufactureOrderItem.PreviousCycleTime != control.LatheManufactureOrderItem.CycleTime)
-            //{
-            //    int diff = control.LatheManufactureOrderItem.CycleTime - (int)control.LatheManufactureOrderItem.PreviousCycleTime;
-            //    string f;
-            //    string b;
-            //    string pd;
-
-            //    if (diff <= 0)
-            //    {
-            //        f = "Teal";
-            //        b = "TealFaded";
-            //        pd = "M10,4H14V13L17.5,9.5L19.92,11.92L12,19.84L4.08,11.92L6.5,9.5L10,13V4Z";
-            //    }
-            //    else
-            //    {
-            //        f = "Red";
-            //        b = "RedFaded";
-            //        pd = "M14,20H10V11L6.5,14.5L4.08,12.08L12,4.16L19.92,12.08L17.5,14.5L14,11V20Z";
-            //    }
-
-            //    control.deltaIndicator.Background = (Brush)Application.Current.Resources[b];
-            //    control.deltaSymbol.Fill = (Brush)Application.Current.Resources[f];
-            //    control.deltaSymbol.Data = Geometry.Parse(pd);
-            //    control.deltaIndicatorText.Foreground = (Brush)Application.Current.Resources[f];
-
-            //    control.deltaIndicatorText.Text = Math.Abs(diff).ToString("0");
-            //}
-            //else
-            //{
-            //    control.deltaIndicator.Visibility = Visibility.Collapsed;
-            //}
-            
+            control.CycleTimeNotPresentIndicator.Visibility = control.Item.CycleTime == 0 && control.Item.QuantityMade > 0
+                ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public DisplayLMOItems()
