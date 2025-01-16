15acf5e243c1674ee698f39114690d85f31e760f
Xavier Iafrate <xavieriafrate@gmail.com>
2025-01-16 09:42:33 +0000

    Update new root

--- CloseManufactureOrders/ApplicationRootPaths.cs
+++ CloseManufactureOrders/ApplicationRootPaths.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectLighthouse
+﻿namespace ProjectLighthouse
 {
     public class ApplicationRootPaths
     {
-        public const string DEBUG_ROOT = @"\\groupfile01\Sales\Production\Administration\Manufacture Records\Lighthouse_TEST\";
-        public const string DEBUG_DB_NAME = @"lightroom_TEST.db3";
+        public const string DEBUG_ROOT = @"\\GBCH-LIGHT-01\Lighthouse_Dev\";
+        public const string DEBUG_DB_NAME = @"lightroom_Dev.db3";
 
         public const string RELEASE_ROOT = @"\\GBCH-LIGHT-01\Lighthouse\";
         public const string RELEASE_DB_NAME = @"lightroom_v2.db3";
