f20178f24e818e10c27bba25628311ae8d1df090
Xavier Iafrate <xavieriafrate@gmail.com>
2025-01-17 16:23:56 +0000

    New command

---
+++ ProjectLighthouse/ViewModel/Commands/Scheduling/DeleteMaintenanceEventCommand.cs
@@ -0,0 +1,31 @@
+﻿using ProjectLighthouse.Model.Administration;
+using ProjectLighthouse.Model.Scheduling;
+using ProjectLighthouse.ViewModel.Orders;
+using System;
+using System.Windows.Input;
+
+namespace ProjectLighthouse.ViewModel.Commands.Scheduling
+{
+    public class DeleteMaintenanceEventCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private ScheduleViewModel viewModel;
+
+        public DeleteMaintenanceEventCommand(ScheduleViewModel vm)
+        {
+            this.viewModel = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return App.CurrentUser.HasPermission(Model.Core.PermissionType.ConfigureMaintenance);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter is not MachineService service) return;
+            viewModel.CancelService(service);
+        }
+    }
+}
