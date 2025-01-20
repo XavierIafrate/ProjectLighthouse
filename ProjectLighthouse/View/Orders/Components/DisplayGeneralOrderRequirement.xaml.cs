e02dceaf83a53f69c1865a0de3fc224adecabfc4
Xavier Iafrate <xavieriafrate@gmail.com>
2025-01-20 09:45:17 +0000

    Requirement badge turns green when delivered

--- ProjectLighthouse/View/Orders/Components/DisplayGeneralOrderRequirement.xaml.cs
+++ ProjectLighthouse/View/Orders/Components/DisplayGeneralOrderRequirement.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectLighthouse.Model.Orders;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ProjectLighthouse.View.Orders.Components
 {
@@ -13,8 +14,31 @@ namespace ProjectLighthouse.View.Orders.Components
         }
 
         public static readonly DependencyProperty OrderProperty =
-            DependencyProperty.Register("Order", typeof(GeneralManufactureOrder), typeof(DisplayGeneralOrderRequirement), new PropertyMetadata(null));
+            DependencyProperty.Register("Order", typeof(GeneralManufactureOrder), typeof(DisplayGeneralOrderRequirement), new PropertyMetadata(null, SetUI));
 
+        private static void SetUI(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is not DisplayGeneralOrderRequirement control) return;
+            if (control.Order is null) return;
+
+            string colourForeground = "Red";
+            string colourBackground = "RedFaded";
+
+            if (control.Order.RequiredQuantity > 0 && control.Order.DeliveredQuantity >= control.Order.RequiredQuantity)
+            {
+                colourBackground = "GreenFaded";
+                colourForeground = "Green";
+            }
+
+            Brush fg = (Brush)Application.Current.Resources[colourForeground];
+            Brush bg = (Brush)Application.Current.Resources[colourBackground];
+
+            control.RequirementBadge.Background = bg;
+            control.RequirementBadge.BorderBrush = fg;
+
+            control.QuantityRequiredTextBlock.Foreground = fg;
+            control.DateRequiredTextBlock.Foreground = fg;
+        }
 
         public DisplayGeneralOrderRequirement()
         {
